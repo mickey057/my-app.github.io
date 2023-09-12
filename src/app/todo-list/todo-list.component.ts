23bfb920d0cc3475c21b094677bc9ba707d5d8c5
Mickey N <ntwari83@gmail.com>
2023-09-12 11:00:55 +0200

    to do app finished

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
-  styleUrls: ['./todo-list.component.css']
+  styleUrls: ['./todo-list.component.css'],
 })
 export class TodoListComponent {
+  taskArray = [{ taskName: 'Brush teeth', iscompleted: false }];
+  onSubmit(form: NgForm) {
+    console.log(form);
 
+    this.taskArray.push({
+      taskName: form.controls['task'].value,
+      iscompleted: false,
+    });
+    form.reset();
+  }
+  onDelete(index: number) {
+    console.log(index);
+
+    this.taskArray.splice(index, 1);
+  }
+  oncheck(index: number){
+    console.log(this.taskArray);
+
+    this.taskArray[index].iscompleted = !this.taskArray[index].iscompleted;
+  }
 }
+
